c453cd7dcc7d34614d8a7d8c9e5a80a9a7dd25c3
VicTorM <vmiguelg@gmail.com>
2025-12-21 03:59:45 -0500

    mejorando sistema 1

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -3,6 +3,7 @@
 
 <h1>Bienvenido al Panel</h1>
 <p>¡Has iniciado sesión correctamente!</p>
+<h2>aqui más info</h2>
 <img src="https://sweetalert2.github.io/images/swal2-modal.png" alt="SweetAlert2" width="300"/>
 
 <?= $this->endSection() ?>
